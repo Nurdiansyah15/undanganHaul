3b8aed509f786654d3cfc7ddd15576c6c567b284
Nurdiansyah15 <nurdiansyahmrnurd150900@gmail.com>
2021-03-27 20:52:52 +0700

    membuat komentar

--- script.js
+++ script.js
@@ -33,3 +33,60 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+// komentar
+$(document).ready(function(){
+  //Mengirimkan Token Keamanan
+  $.ajaxSetup({
+          headers : {
+              'Csrf-Token': $('meta[name="csrf-token"]').attr('content')
+          }
+      });
+    
+  $('#form_komen').on('submit', function(event){
+    event.preventDefault();
+    let nama_pengirim = $('#nama_pengirim').val();
+    let komen = $('#komen').val();
+    
+    if(nama_pengirim==''){
+        alert("Nama Pengirim Harus disii");
+    } else if(komen==''){
+        alert("Komentar Harus disii");
+    } else {
+        var form_data = $(this).serialize();
+        $.ajax({
+          url:"tambah_komentar.php",
+          method:"POST",
+          data:form_data,
+          success:function(data){
+            $('#form_komen')[0].reset();
+            $('#komentar_id').val('0');
+            load_comment();
+          }, error: function(data) {
+                  console.log(data.responseText)
+                }
+        })
+    }
+  });
+
+  load_comment();
+
+  function load_comment(){
+    $.ajax({
+      url:"ambil_komentar.php",
+      method:"POST",
+      success:function(data){
+        $('#display_comment').html(data);
+      }, error: function(data) {
+              console.log(data.responseText)
+            }
+    })
+  }
+
+  $(document).on('click', '.reply', function(){
+    var komentar_id = $(this).attr("id");
+    $('#komentar_id').val(komentar_id);
+    $('#nama_pengirim').focus();
+  });
+});
+// komentar
